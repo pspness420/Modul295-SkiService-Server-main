05021065596c2f0e9d65f16998c40c19edb917a7
Yannick <yannick.frei@student.ipso.ch>
2024-12-09 14:38:54 +0100

    .

--- SkiServiceManagement/obj/Debug/net9.0/SkiServiceManagement.AssemblyInfo.cs
+++ SkiServiceManagement/obj/Debug/net9.0/SkiServiceManagement.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SkiServiceManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f03009a49f3ac4d487161228088e545d1c1e0b58")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f23cd17225e30a68fb80fcd2152d1d69bdd11fcd")]
 [assembly: System.Reflection.AssemblyProductAttribute("SkiServiceManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SkiServiceManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
