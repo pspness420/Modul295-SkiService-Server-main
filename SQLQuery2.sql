aba0c14eb6b7c33984e0b476d9a2162054e4d2a2
Yannick <yannick.frei@student.ipso.ch>
2024-12-10 10:12:33 +0100

    .

---
+++ SQLQuery2.sql
@@ -0,0 +1,16 @@
+UPDATE Benutzer SET Rolle = 'Kunde' WHERE Rolle IS NULL;
+SELECT TOP (1000) [Id]
+      ,[Benutzername]
+	  ,[Email]
+      ,[Passwort]
+      ,[Rolle]
+  FROM [SkiServiceDB].[dbo].[Benutzer]
+
+SELECT TOP (1000) [Id]
+      ,[KundenName]
+      ,[Email]
+      ,[Telefon]
+      ,[Prioritaet]
+      ,[Dienstleistung]
+      ,[Status]
+  FROM [SkiServiceDB].[dbo].[Serviceauftraege]
